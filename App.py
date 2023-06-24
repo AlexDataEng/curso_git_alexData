abd412da5af323844de4e967a327be7043f79029
Alejandro Castro <jalejandro.ecom@gmail.com>
2023-06-24 18:28:04 -0400

    Version 0.0.2

--- App.py
+++ App.py
@@ -1,8 +1,12 @@
 from funciones.Bienvenida import Bienvenidas
+from funciones.Despedida import Despedida
 from decouple import config
 
 #print(config("nombre_secreto"))
 
 bienvenida = Bienvenidas("Alejandro")
 
+despedida = Despedida("Mirla")
+
 bienvenida.dias()
+despedida.chao()
---
+++ funciones/Despedida.py
@@ -0,0 +1,6 @@
+class Despedida():
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def chao(self):
+        print(f"Hasta luego {self.nombre}")
